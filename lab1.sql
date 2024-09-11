6cca7bd8b9947cdedd212e1d6860d4476271b737
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-09-11 18:41:53 +0500

    first commit

---
+++ lab1.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE lab1;
+
+create table users(
+	id SERIAL,
+	firstname VARCHAR(50),
+	lastname VARCHAR(50)
+);
+
+ALTER TABLE users ADD COLUMN isadmin SMALLINT;
+
+ALTER TABLE users
+ALTER COLUMN isadmin SET DATA TYPE boolean USING (isadmin = 1);;
+
+ALTER TABLE users
+ALTER COLUMN isadmin SET DEFAULT FALSE;
+
+ALTER TABLE users ADD PRIMARY KEY(id);
+
+create table tasks (
+	id SERIAL,
+	name VARCHAR(50),
+	user_id INT
+);
+
+DROP TABLE tasks;
+
+DROP DATABASE lab1;
+
+
+
+
+
